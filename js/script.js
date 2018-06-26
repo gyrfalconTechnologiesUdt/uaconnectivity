35700da525d6a0dfbade14ee6686ce31ad23118c
saranyaveni <saranyaveni28@gmail.com>
2018-06-26 18:00:17 +0530

    changes updated

---
+++ js/script.js
@@ -0,0 +1,81 @@
+$(document) .ready(function(){
+	
+
+	
+$("#userNameInput input").focus(function(e){
+	
+$("#userNameLbl").addClass("userNameLbl");
+
+});
+$("#userNameInput input").blur(function(e){
+	
+$("#userNameLbl").removeClass("userNameLbl");
+
+});
+
+$("#userNameInput input").focus(function(e){
+	
+$("#emailIcon").addClass("emailIcon");
+
+});
+$("#userNameInput input").blur(function(e){
+	
+$("#emailIcon").removeClass("emailIcon");
+
+});
+$("#passwordinput input").focus(function(e){
+	
+$("#passwordLbl").addClass("passwordLbl");
+
+});
+$("#passwordinput input").blur(function(e){
+	
+$("#passwordLbl").removeClass("passwordLbl");
+
+});
+$("#passwordinput input").focus(function(e){
+	
+$("#passwordIcon").addClass("passwordIcon");
+
+});
+$("#passwordinput input").blur(function(e){
+	
+$("#passwordIcon").removeClass("passwordIcon");
+
+});
+
+
+
+$("#contactNoinput input").focus(function(e){
+	
+$("#contNoLbl").addClass("contNoLbl");
+
+});
+$("#contactNoinput input").blur(function(e){
+	
+$("#contNoLbl").removeClass("contNoLbl");
+
+});
+$("#confirmPasswordinput input").focus(function(e){
+	
+$("#cPasswordLbl").addClass("cPasswordLbl");
+
+});
+$("#confirmPasswordinput input").blur(function(e){
+	
+$("#cPasswordLbl").removeClass("cPasswordLbl");
+
+});
+$("#emailIdinput input").focus(function(e){
+	
+$("#emailIdLbl").addClass("emailIdLbl");
+
+});
+$("#emailIdinput input").blur(function(e){
+	
+$("#emailIdLbl").removeClass("emailIdLbl");
+
+});
+
+
+});
