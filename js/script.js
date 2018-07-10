0be048217c2f97c9e2aafc6bc51889082b9e30da
Gayathri <gayathriloganathan18@gmail.com>
2018-07-10 13:16:18 +0530

    changes updated

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(document) .ready(function(){
 	
-
-	
-$("#userNameInput input").focus(function(e){
-	
+$("#userNameInput input").focus(function(e){	
 $("#userNameLbl").addClass("userNameLbl");
-
 });
-$("#userNameInput input").blur(function(e){
-	
-$("#userNameLbl").removeClass("userNameLbl");
 
-});
+$("#userNameInput input").blur(function(e){	
+	let inputVal = e.currentTarget.value;
+	if(inputVal === ''){
+		$("#userNameLbl").removeClass("userNameLbl");
+		
+	}
+});	
+
 
 $("#userNameInput input").focus(function(e){
 	
@@ -23,16 +23,18 @@ $("#userNameInput input").blur(function(e){
 $("#emailIcon").removeClass("emailIcon");
 
 });
-$("#passwordinput input").focus(function(e){
-	
+$("#passwordinput input").focus(function(e){	
 $("#passwordLbl").addClass("passwordLbl");
-
 });
-$("#passwordinput input").blur(function(e){
-	
-$("#passwordLbl").removeClass("passwordLbl");
 
-});
+$("#passwordinput input").blur(function(e){	
+	let inputVal = e.currentTarget.value;
+	if(inputVal === ''){
+		$("#passwordLbl").removeClass("passwordLbl");
+		
+	}
+});	
+
 $("#passwordinput input").focus(function(e){
 	
 $("#passwordIcon").addClass("passwordIcon");
@@ -44,38 +46,42 @@ $("#passwordIcon").removeClass("passwordIcon");
 
 });
 
-
-
-$("#contactNoinput input").focus(function(e){
-	
+// ua sign up page start
+$("#contactNoinput input").focus(function(e){	
 $("#contNoLbl").addClass("contNoLbl");
-
 });
-$("#contactNoinput input").blur(function(e){
-	
-$("#contNoLbl").removeClass("contNoLbl");
 
-});
-$("#confirmPasswordinput input").focus(function(e){
-	
+$("#contactNoinput input").blur(function(e){	
+	let inputVal = e.currentTarget.value;
+	if(inputVal === ''){
+		$("#contNoLbl").removeClass("contNoLbl");
+		
+	}
+});	
+
+
+$("#confirmPasswordinput input").focus(function(e){	
 $("#cPasswordLbl").addClass("cPasswordLbl");
-
 });
-$("#confirmPasswordinput input").blur(function(e){
-	
-$("#cPasswordLbl").removeClass("cPasswordLbl");
 
-});
-$("#emailIdinput input").focus(function(e){
-	
+$("#confirmPasswordinput input").blur(function(e){	
+	let inputVal = e.currentTarget.value;
+	if(inputVal === ''){
+		$("#cPasswordLbl").removeClass("cPasswordLbl");
+		
+	}
+});	
+
+$("#emailIdinput input").focus(function(e){	
 $("#emailIdLbl").addClass("emailIdLbl");
-
-});
-$("#emailIdinput input").blur(function(e){
-	
-$("#emailIdLbl").removeClass("emailIdLbl");
-
 });
 
+$("#emailIdinput input").blur(function(e){	
+	let inputVal = e.currentTarget.value;
+	if(inputVal === ''){
+		$("#emailIdLbl").removeClass("emailIdLbl");
+		
+	}
+});	
 
 });
